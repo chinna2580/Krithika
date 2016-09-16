1c6ac5efea470e7185949ebbe0334aab709f4804
Chinna2580 <chinna2580@gmail.com>
2016-09-16 18:47:36 +0530

    static with usermodels1

--- static_app/app/models/user.rb
+++ static_app/app/models/user.rb
@@ -5,4 +5,8 @@ before_save { email.downcase! }
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
+					
+					
 end
